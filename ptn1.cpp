c2e81c938307e1fc73386da4f704c24a3abd9399
Gaurav Kumar <gauravsony67@gmail.com>
2025-02-21 01:58:18 +0530

    pritn17 added

--- ptn1.cpp
+++ ptn1.cpp
@@ -182,13 +182,40 @@ void print10(int n){
             cout<<endl;
         }
     }
+     void print16(int n){
+        for(int i=1; i<=n; i++){
+            char ch = 'A'+(i-1);
+            for(int j=1; j<=i; j++){
+            cout<<ch<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    void print17(int n){
+    for(int i=1; i<=n; i++){
+         for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }//space
+        char ch;
+        for(int j=1; j<=(2*i)-1; j++){
+            if(j<=i){
+                ch = 'A'+(j-1);
+                cout<<ch;
+            }else{
+                ch--;
+                cout<<ch;
+            }
+        }
+        cout<<endl;
+    }
+    }
 int main(){
 int t;
 cin>>t;
 for(int i=0; i<t; i++){
     int n;
     cin>>n;
-  print15(n);
+  print17(n);
 
 }
 }
